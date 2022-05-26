7e7b21c503ec38374c286ce2ca91de70ed859894
mikealian <106313030+mikealian@users.noreply.github.com>
2022-05-26 21:44:30 +0800

    Add files via upload

---
+++ CLE/index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Michael Remegio Alian Portfolio</title>
+       
+        <link rel="icon" type="image/x-icon" href="assets/mikelogo.png" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <img style ="height: 50px;width: 60px" src="assets/mikelogo.png" alt="..." />
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#myworks">My Works</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#other">Projects</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <section class="" id="home"></section>
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Michael Remegio Alian</h1>
+                        <hr class="divider" />
+                        
+                        <h2 class="text-white mt-0">BSIT - 1C</h2>
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                    </div>
+                </div>
+            </div>
+        </header>
+    </section>
+        <!-- Home-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Facts about me</h2>
+                        
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4" style = "font-size:25px">I'm 20 years old<br>
+                            I'm From Escalante City<br>
+                            Currently studying at University of St. La Salle<br></p>
+                            <hr class="divider divider-light" />
+                            <p class="text-white-75 mb-4" style = "font-size:25px"> I love dogs.<br>
+                            I like watching Star Wars.<br>
+                            I love doing arts.<br>
+                            I am very interested in talking about history.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Skills-->
+        <section class="page-section" id="skills">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">SKILLS</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-tablet fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Digital Art</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Photo/video editing</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Editorial Cartooning</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Playing Musical instruments</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- My Works-->
+        <div id="myworks">
+            <div class="container-fluid p-0">
+                    <h2 class="text-center mt-0">MY WORKS</h2>
+                    
+                <hr class="divider" />
+                <div class="row g-0">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/11.jpeg" target="_blank" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/11.jpeg" target="_blank"alt="..." />
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/12.jpeg" target="_blank"title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/12.jpeg" target="_blank"alt="..." />
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/13.jpeg" target="_blank"title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/13.jpeg" target="_blank"lt="..." />
+                            
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/14.jpeg" target="_blank"title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/14.jpeg" target="_blank"alt="..." />
+                            
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/15.jpeg" target="_blank"title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/15.jpeg" target="_blank"alt="..." />
+                            
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/16.jpeg" target="_blank" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/16.jpeg" target="_blank"alt="..." />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+       <!--OTHERS-->
+        <section class="page-section bg-primary" id="other">
+                <div class="container px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5 justify-content-center">
+                        <div class="col-lg-8 text-center">
+                            <h2 class="text-white mt-0">PROJECTS</h2>
+                            <hr class="divider divider-light">
+                            <a class="btn btn-primary btn-xl" href="projects/LT2/index.html" target="_blank" style = "width: 300px; height: 60px;" >Learning Task 2</a><br><br>
+                            <a class="btn btn-primary btn-xl" href="projects/LT3/index.html" target="_blank" style = "width: 300px; height: 60px;">Learning Task 3</a><br><br>
+                            <a class="btn btn-primary btn-xl" href="projects/CLE1MT/index.html" target="_blank" style = "width: 300px; height: 60px;">CLE1MT</a><br><br>
+                            <a class="btn btn-primary btn-xl" href="projects/LT4/index.html" target="_blank" style = "width: 300px; height: 60px;">Learning Task 4</a><br><br>
+                            <a class="btn btn-primary btn-xl" href="projects/LT5/index.html" target="_blank" style = "width: 300px; height: 60px;">Learning Task 5</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+               <!-- My Works-->
+        
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; Michael Remegio Alian</div></div>
+        </footer>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
