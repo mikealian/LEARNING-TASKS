5544d0eb37f2a9d1d7a2973e607dcc3ceaaa1bf7
mikealian <106313030+mikealian@users.noreply.github.com>
2022-05-26 20:29:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+          <title> My Home Page </title>
+    </head>
+    <body>
+    <h1> Welcome to my Website! I'm Rheyxs Fernandez </h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="LT2/recipe.html">Learning Task 2</a></li>
+        <li><a href="LT3">Learning Task 3</a></li>
+        <li><a href="CLE1MT">CLE1MT</a></li>
+        <li><a href="LT4">Learning Task 4</a></li>
+        <li><a href="LT5">Learning Task 5</a></li>
+    </ul>
+    </body>
+</html>
